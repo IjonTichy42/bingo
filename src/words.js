b4b83c5534196c3fd68a4cdf3ebd8420996df9d0
Valerii Sedin <vvsedin@yandex.ru>
2023-03-03 12:49:22 +0400

    Add hybrid schedule to words

--- src/words.js
+++ src/words.js
@@ -1,3 +1,3 @@
-const words = ["team", "loyal", "funding", "shares", "Dragonmart", "MVP", "investors", "February salary will be in time", "delays", "fines", "bankrupcy", "family", "my situation even worse", "1-3 seconds  for q&a", "issues with bank"];
+const words = ["team", "loyal", "funding", "shares", "Dragonmart", "MVP", "investors", "February salary will be in time", "delays", "fines", "bankrupcy", "family", "my situation even worse", "1-3 seconds  for q&a", "issues with bank", "hybrid schedule"];
 
 export default words;

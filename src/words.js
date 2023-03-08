5c3e8a2b15be273d3639052580f4ce3941afca9d
Valerii Sedin <vvsedin@yandex.ru>
2023-03-08 13:02:06 +0400

    Add words

--- src/words.js
+++ src/words.js
@@ -1,3 +1,3 @@
-const words = ["team", "loyal", "funding", "shares", "Dragonmart", "MVP", "investors", "February salary will be in time", "delays", "fines", "bankrupcy", "family", "my situation even worse", "1-3 seconds  for q&a", "issues with bank", "hybrid schedule"];
+const words = ["team", "loyal", "funding", "shares", "Dragonmart", "MVP", "investors", "February salary will be in time", "delays", "fines", "bankrupcy", "family", "my situation even worse", "1-3 seconds  for q&a", "issues with bank", "hybrid schedule", "bridge loan", "ну", "escalate", "discuss separately"];
 
 export default words;
